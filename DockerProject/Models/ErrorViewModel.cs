7f7e8464a67aa8abd09672b61b23b9a3ef7a353b
mtmonir <tahir.monir@gmail.com>
2018-09-19 20:04:22 -0500

    Add project files.

---
+++ DockerProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DockerProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
